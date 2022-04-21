4f4ffb0b2a7c6f58058be67f4c9bd442358af3f9
Abirami2329 <96733813+Abirami2329@users.noreply.github.com>
2022-04-21 17:20:02 +0530

    Update TicTacToe.java

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    public static int[][] winningSequences = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
+    public final static int[][] winningSequences = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
 
     public enum Evaluation {
 
