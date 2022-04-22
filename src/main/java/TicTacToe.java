d340a96dcb9f7f888beb0aac14f8860d0e80e562
Abirami2329 <96733813+Abirami2329@users.noreply.github.com>
2022-04-22 10:59:38 +0530

    Update TicTacToe.java

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -10,7 +10,7 @@ public class TicTacToe {
 
     }
 
-    public static Evaluation evaluateBoard (String board){
+    public static Evaluation evaluateBoard (int board){
 
         if (board.length() != 9){
 
